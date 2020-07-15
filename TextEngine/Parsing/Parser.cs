29afc40c078c7813a1035cf7b6faa632e7c3b1ca
Chris Anders <filmee24@gmail.com>
2020-07-15 21:21:29 +0200

    Added core for parser

---
+++ TextEngine/Parsing/Parser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using TextEngine.Parsing.Text;
+
+namespace TextEngine.Parsing
+{
+    public class Parser
+    {
+        private ImmutableArray<Token> _tokens;
+        private int _position;
+
+        public SyntaxNode Parse(string src, string filename = "default.script")
+        {
+            var lexer = new Lexer(SourceText.From(src, filename));
+           
+            _tokens = lexer.GetAllTokens().ToImmutableArray();
+
+            return null;
+        }
+
+        private Token Peek(int offset)
+        {
+            var index = _position + offset;
+            if (index >= _tokens.Length)
+                return _tokens[^1];
+
+            return _tokens[index];
+        }
+
+        private Token Current => Peek(0);
+
+        private Token NextToken()
+        {
+            var current = Current;
+            _position++;
+            return current;
+        }
+
+        private Token MatchToken(SyntaxKind kind)
+        {
+            if (Current.Kind == kind)
+                return NextToken();
+
+            return new Token(kind, Current.Position, null, null);
+        }
+    }
+}
